8ffb84dc48b356156103220d37a4e0e3426b985e
Patrick Brebner <patrick.brebner@gmail.com>
2023-03-05 16:45:33 -0800

    Initial commit

---
+++ binary-search-trees/binarySearchTrees.js
@@ -0,0 +1,11 @@
+// Practice building binary search trees
+
+class Tree {
+    constructor(array) {
+        this.array = array;
+    }
+}
+
+const nodeFactory = (data, leftNode, rightNode) => {
+    return { data };
+};
